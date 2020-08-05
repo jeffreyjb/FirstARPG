2419e4af15898db41064637efb9e8200394c9081
Jeff Brown <jeffjjbrown@gmail.com>
2020-08-05 15:35:24 -0700

    Update to start with TopDown Blueprint Template

--- Source/FirstARPG/FirstARPG.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstARPG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstARPG, "FirstARPG" );
-
-DEFINE_LOG_CATEGORY(LogFirstARPG)
- 
